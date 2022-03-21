72e5e9c1aa411f3a9707f19e84c3bd8330659781
Temirhan <qwe.mister.qwe@bk.ru>
2022-03-22 00:34:26 +0600

    добавление слайда v.0.0.1

--- js/script.js
+++ js/script.js
@@ -42,3 +42,47 @@ new Swiper('.image-slider',{
     speed: 800,
     
 });
+new Swiper('.specification__slider',{
+    // Стрелки
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+    // Навигация
+    // Буллеты, текущее положение, прогресс бар
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    // Скролл
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //     draggable: true,
+    // }
+    // Включение/отключение перетаскивание на ПК
+    simulateTouch: true,
+    // Чувствительность свайпа 
+    touchratio: 1,
+    // Угол срабатывание 
+    touchangle: 45,
+    // Курсор перетаскиване
+    grabCursor: true,
+    // Бесконечный слайд
+    loop: true,
+    slidesPerView: 'auto',
+
+    // Автопрокрутка 
+    autoplay: {
+        // Пауза между прокруткой
+        delay: 5000, // 5 sec
+        // Закончить на последнем слайде 
+        stopOnLastSlide: false, 
+        // Отключить после ручного переключения
+        disableOnInteraction: false,
+    },
+
+    // Скорость переключения 
+    speed: 800,
+    
+});
